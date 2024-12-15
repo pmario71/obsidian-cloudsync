44bd97b48aedea8024476236d37e80d539990804
Miha Kralj <miha.kralj@outlook.com>
2024-12-14 20:47:46 -0800

    Add diff-match-patch module declarations

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,17 +1,15 @@
-declare module "*.js" {
-    const content: any;
-    export = content;
-}
-
-// Declare provider modules
-declare module "./aws-provider.js" {
-    export { AWSManager } from "./AWSManager";
-}
-
-declare module "./azure-provider.js" {
-    export { AzureManager } from "./AzureManager";
-}
-
-declare module "./gcp-provider.js" {
-    export { GCPManager } from "./GCPManager";
+declare module 'diff-match-patch' {
+    export class diff_match_patch {
+        constructor();
+        Diff_Timeout: number;
+        Match_Threshold: number;
+        Patch_DeleteThreshold: number;
+        static readonly DIFF_DELETE: -1;
+        static readonly DIFF_INSERT: 1;
+        static readonly DIFF_EQUAL: 0;
+        diff_main(text1: string, text2: string, checkLines?: boolean): Array<[number, string]>;
+        diff_linesToChars_(text1: string, text2: string): {chars1: string, chars2: string, lineArray: string[]};
+        diff_charsToLines_(diffs: Array<[number, string]>, lineArray: string[]): void;
+        diff_cleanupSemantic(diffs: Array<[number, string]>): void;
+    }
 }
